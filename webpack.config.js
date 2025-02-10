e455fe2ef10142924b9d0ffc5b0104d12cd4c960
Carlos Eduardo <c.edu31@hotmail.com>
2025-02-10 19:40:28 -0300

    install and config json server + webpack + webpack serve, add on package.json scripts exe, author and description

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+
+  entry: path.resolve(__dirname, 'src', 'main.js'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    liveReload: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, 'index.html')
+      }
+    )
+  ],
+};
