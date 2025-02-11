cf5b683fbea8bf3cf0fd6fa44122f63acbc46b9c
Carlos Eduardo <c.edu31@hotmail.com>
2025-02-11 19:03:15 -0300

    add babel and dayjs

---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import './libs/dayjs.js'
+
+import './styles/form.css'
+import './styles/global.css'
+import './styles/schedule.css'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin"); 
 
 module.exports = {
   target: 'web',
@@ -27,6 +28,33 @@ module.exports = {
         template: path.resolve(__dirname, 'index.html'),
         favicon: path.resolve('src', 'assets', 'scissors.svg')
       }
-    )
+    ),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets'),
+          to: path.resolve(__dirname, 'dist', 'src', 'assets')
+        }
+      ]
+    })
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      }
+    ]
+  }
 };
