8bd0cfe2c627c11e6776c28104c008fbec137327
Carlos Eduardo <c.edu31@hotmail.com>
2025-02-12 20:05:50 -0300

    input date config by dayjs

--- src/main.js
+++ src/main.js
@@ -6,3 +6,5 @@ import './styles/form.css'
 import './styles/global.css'
 import './styles/schedule.css'
 
+import './modules/form/submit.js'
+
---
+++ src/modules/form/submit.js
@@ -0,0 +1,18 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector('form')
+const selectedDate = document.getElementById('date')
+
+//Define a data atual
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD')
+
+//Data atual
+selectedDate.value = inputToday
+
+//Define a data minima como a data atual
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log("Enviado")
+}
