2b9d5ce141e0797d2db8cdc2026cd256f1248476
Carlos Eduardo <c.edu31@hotmail.com>
2025-02-24 21:56:43 -0300

    show schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -2,6 +2,8 @@ import { hoursLoad } from "../form/hours-load.js";
 import {scheduleFetchByDay} from '../../../services/schedule-fetch-by-day.js'
 const selectedDate = document.getElementById('date')
 
+import {scheduleShow} from '../schedules/show.js'
+
 export async function schedulesDay () {
   //Obtém a data do input
   const date = selectedDate.value
@@ -9,7 +11,9 @@ export async function schedulesDay () {
   //Buscar os horários agendados na API
   const dailySchedules = await scheduleFetchByDay({ date })
 
-  console.log(dailySchedules)
+  //Renderizar/exbir os agendamentos
+  scheduleShow({dailySchedules})
+
   //Renderiza as horas disponiveis
   hoursLoad({date})
 } 
